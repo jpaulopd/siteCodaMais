59dbbb62636e3436bbb85508d6a51206b7448b79
JOAO PAULO PIRES DANTAS <joaod3v@gmail.com>
2020-04-01 21:08:43 -0300

    ajuste para reposividade em aplicações mobile

--- versao3/js/init.js
+++ versao3/js/init.js
@@ -4,6 +4,11 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.slider').slider();
+  //   $('.slider').slider({ 
+  //     // height : 250,
+  //     width : 580 
+  // });
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
     $('.collapsible').collapsible();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
